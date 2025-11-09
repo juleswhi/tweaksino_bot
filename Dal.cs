3e9e70d0e104f5029d0247c0e0ec8eb8d487f326
callum white <callum@callumwhite.dev>
2025-11-09 21:32:06 +0100

    batman

---
+++ Dal.cs
@@ -0,0 +1,195 @@
+using Microsoft.Data.Sqlite;
+
+public interface IDatabaseModel {}
+
+public class Bitch : IDatabaseModel {
+    [PrimaryKey]
+    public string Id { get; set; } = "";
+    public double Money { get; set; } = 0;
+    public string Password { get; set; } = "";
+}
+
+public class DiscordBitch : IDatabaseModel {
+    [PrimaryKey]
+    public string DiscordId { get; set; } = "";
+    [ForeignKey(typeof(Bitch))]
+    public string BitchId { get; set; } = "";
+}
+
+[AttributeUsage(AttributeTargets.Property)]
+internal class PrimaryKey : Attribute { }
+
+[AttributeUsage(AttributeTargets.Property)]
+internal class ForeignKeyAttribute : Attribute {
+    public Type Type { get; set; } = typeof(IDatabaseModel);
+    public ForeignKeyAttribute(Type type) {
+        Type = type;
+    }
+}
+
+public static class ModelHelper
+{
+    public static IEnumerable<(string, string)> GetPrimaryKey<T>(this T model) where T : IDatabaseModel
+    {
+        foreach (var prop in model.GetType().GetProperties())
+        {
+            var attr = Attribute.GetCustomAttribute(prop, typeof(PrimaryKey));
+            if (attr is null) continue;
+            yield return (prop.Name, (string)prop.GetValue(model)!);
+        }
+    }
+
+    public static string FormatKeys<T>(this T model) where T : IDatabaseModel =>
+        model.GetPrimaryKey().Select(x => $"{x.Item1} = '{x.Item2}'").Aggregate((x, y) => $"{x} AND {y}");
+}
+
+public static class DatabaseLayer
+{
+    /// <summary>
+    /// Connection String for database
+    /// </summary>
+    public static string ConnectionString { get; set; } = "Data Source=../tweaksino/db.db";
+
+    /// <summary>
+    /// Query a database via class (and with selected column names)
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="names"></param>
+    /// <returns></returns>
+    public static List<T> Query<T>() where T : IDatabaseModel
+    {
+        if(ConnectionString == "")
+        {
+            throw new Exception("Please populate the ConnectionString property");
+        }
+
+        var type = typeof(T);
+
+        using var conn = new SqliteConnection($"{ConnectionString}");
+        conn.Open();
+        using var command = new SqliteCommand($"select * from {type.Name} ;", conn);
+
+        // Grab results from query
+        using var reader = command.ExecuteReader();
+
+        List<T> results = [];
+
+        // This will loop through all rows in query
+        while (reader.Read())
+        {
+            // Create instance of the DatabaseModel
+            T obj = Activator.CreateInstance<T>();
+            foreach (var property in type.GetProperties())
+            {
+                // Grab the column cooresponding to the name
+                var ord = reader.GetOrdinal(property.Name);
+
+                var t = property.PropertyType;
+
+                // Properly cast to type based on SQL type
+                switch (property.PropertyType.Name)
+                {
+                    case "Int32":
+                    case "System.Int32":
+                        var num = reader.GetInt32(ord);
+                        var prop = type.GetProperty(property.Name);
+                        if (prop is null)
+                            break;
+                        prop.SetValue(obj, num);
+                        break;
+                    case "Double":
+                        var doub = reader.GetInt32(ord);
+                        var p = type.GetProperty(property.Name);
+                        if (p is null)
+                            break;
+                        p.SetValue(obj, doub);
+                        break;
+                    case "DateTime":
+                        var dt = reader.GetDateTime(ord);
+                        type.GetProperty(property.Name)!.SetValue(obj, dt);
+                        break;
+                    case "Boolean":
+                        var b = reader.GetBoolean(ord);
+                        type.GetProperty(property.Name)!.SetValue(obj, b);
+                        break;
+                    default:
+                        var str = reader.GetString(ord);
+                        type.GetProperty(property.Name)!.SetValue(obj, str);
+                        break;
+                };
+            }
+            results.Add(obj);
+        }
+
+        conn.Close();
+
+        return results;
+    }
+
+
+    /// <summary>
+    /// Update SQL statement
+    /// </summary>
+    /// <typeparam name="T">The type of DatabaseModel to update</typeparam>
+    /// <param name="obj">The instance of DatabaseModel to update</param>
+    /// <returns></returns>
+    public static bool Update<T>(this T obj) where T : IDatabaseModel
+    {
+        var type = typeof(T);
+
+        using var conn = new SqliteConnection($"{ConnectionString}");
+        conn.Open();
+
+        string updates = type.GetProperties().Select(x => x.Name)
+            .Zip(type.GetProperties()
+                .Select(x => x.GetValue(obj))
+                .Select(y => y == null ? "" : y.ToString()))
+            .Select((c, _) => $"{c.First} = '{c.Second}'")
+            .Aggregate((x, y) => $"{x}, {y}");
+
+        using var command = new SqliteCommand($"update {type.Name} set {updates} where {obj.FormatKeys()};", conn);
+        int res = command.ExecuteNonQuery();
+
+        conn.Close();
+
+        return res == 0;
+    }
+
+    public static bool Delete<T>(this T obj) where T : IDatabaseModel
+    {
+        var type = typeof(T);
+
+        using var conn = new SqliteConnection($"{ConnectionString}");
+        conn.Open();
+
+        using var command = new SqliteCommand($"delete from {type.Name} where {obj.FormatKeys()};", conn);
+        return command.ExecuteNonQuery() == 0;
+    }
+
+    public static bool Create<T>(this T obj) where T : IDatabaseModel
+    {
+        var type = typeof(T);
+        using var conn = new SqliteConnection($"{ConnectionString}");
+        conn.Open();
+
+        var props = type.GetProperties();
+        var names = props.Select(x => x.Name);
+        var aggr = names.Aggregate((x, y) => $"{x}, {y}");
+        string vals = type.GetProperties().
+            Select(x => x.GetValue(obj)).
+            Select(x => x!.ToString()).
+            // Select(x => x!.Replace("'", "''")).
+            Aggregate((x, y) => {
+                    return $"{x}, '{y}'";
+                })!;
+
+        var cmd = $"insert into {type.Name} ({aggr}) values ({vals})";
+
+        Console.WriteLine(cmd);
+
+        using var command = new SqliteCommand(cmd, conn);
+
+        return command.ExecuteNonQuery() == 0;
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,195 @@
+﻿public static class Program {
+    private static Discord.WebSocket.DiscordSocketClient _client = new();
+
+    public static async Task Main() {
+        _client.Log += Log;
+
+        var token = "";
+
+        await _client.LoginAsync(Discord.TokenType.Bot, token);
+        await _client.StartAsync();
+
+        _client.Ready += ClientReady;
+        _client.SlashCommandExecuted += SlashCommandHandler;
+
+        await Task.Delay(-1);
+    }
+
+    public static async Task ClientReady() {
+
+        var register = new Discord.SlashCommandBuilder()
+            .WithName("register")
+            .WithDescription("Register Discord Account To Bank Account")
+            .AddOption("id", Discord.ApplicationCommandOptionType.String, "Id", isRequired: true)
+            .AddOption("password", Discord.ApplicationCommandOptionType.String, "Password", isRequired: true);
+
+        var balance = new Discord.SlashCommandBuilder()
+            .WithName("balance")
+            .WithDescription("Get Account Balance")
+            .AddOption("id", Discord.ApplicationCommandOptionType.String, "Balance of account", isRequired: false);
+
+        var send = new Discord.SlashCommandBuilder()
+            .WithName("send")
+            .WithDescription("Send Money To Someone")
+            .AddOption("user", Discord.ApplicationCommandOptionType.User, "User to send to", isRequired: true)
+            .AddOption("amount", Discord.ApplicationCommandOptionType.Number, "Amount to send", isRequired: true);
+
+        try {
+            await _client.CreateGlobalApplicationCommandAsync(register.Build());
+            await _client.CreateGlobalApplicationCommandAsync(balance.Build());
+            await _client.CreateGlobalApplicationCommandAsync(send.Build());
+        } catch(ApplicationException ex) {
+            _ = ex;
+            Console.WriteLine("error");
+        }
+    }
+
+    private static async Task SlashCommandHandler(Discord.WebSocket.SocketSlashCommand cmd) {
+        switch (cmd.Data.Name) {
+            case "register":
+                await RegisterHandler(cmd);
+                break;
+            case "balance":
+                await BalanceHandler(cmd);
+                break;
+            case "send":
+                await SendHandler(cmd);
+                break;
+        };
+
+    }
+
+
+    private static async Task RegisterHandler(Discord.WebSocket.SocketSlashCommand cmd) {
+        var id = (string)cmd.Data.Options.First().Value;
+        var pw = (string)cmd.Data.Options.First(x => x.Name == "password").Value;
+
+        var user_id = cmd.User.Id;
+
+        var db = new DiscordBitch() {
+            DiscordId = user_id.ToString(),
+            BitchId = id
+        };
+
+        if(DatabaseLayer.Query<DiscordBitch>().FirstOrDefault(x => x.BitchId == id) != null) {
+            var error_embed = new Discord.EmbedBuilder()
+                .WithTitle($"Account Already Registered")
+                .WithColor(Discord.Color.Red)
+                .WithCurrentTimestamp();
+
+            await cmd.RespondAsync(embed: error_embed.Build(), ephemeral: true);
+            return;
+        }
+
+        DatabaseLayer.Create<DiscordBitch>(db);
+
+        var embed = new Discord.EmbedBuilder()
+            .WithTitle($"Registered Account")
+            .WithColor(Discord.Color.Blue)
+            .WithCurrentTimestamp();
+
+        await cmd.RespondAsync(embed: embed.Build(), ephemeral: true);
+    }
+
+    private static async Task BalanceHandler(Discord.WebSocket.SocketSlashCommand cmd) {
+        var option = cmd.Data.Options.FirstOrDefault();
+
+        string id = "";
+
+        if(option is not null) {
+            id = (string)option.Value;
+        } else {
+            var db = DatabaseLayer.Query<DiscordBitch>().FirstOrDefault(x => x.DiscordId == cmd.User.Id.ToString());
+            if(db is null) {
+                var error_embed = new Discord.EmbedBuilder()
+                    .WithTitle($"Please Specify an id")
+                    .WithColor(Discord.Color.Green)
+                    .WithCurrentTimestamp();
+
+                await cmd.RespondAsync(embed: error_embed.Build());
+                return;
+            }
+
+            id = db.BitchId;
+        }
+
+        var username = cmd.User.GlobalName;
+
+        var bs = DatabaseLayer.Query<Bitch>();
+        var b = bs.First(x => x.Id == id);
+
+        Console.WriteLine($"{b.Id}");
+
+        var embed = new Discord.EmbedBuilder()
+            .WithTitle($"{username}'s Balance")
+            .WithDescription($"£{b.Money}")
+            .WithColor(Discord.Color.Green)
+            .WithCurrentTimestamp();
+
+        await cmd.RespondAsync(embed: embed.Build());
+    }
+
+    private static async Task SendHandler(Discord.WebSocket.SocketSlashCommand cmd) {
+        var user = (Discord.WebSocket.SocketGuildUser)cmd.Data.Options.First(x => x.Name == "user").Value;
+        var amount = (double)cmd.Data.Options.First(x => x.Name == "amount").Value;
+
+
+        var u1 = DatabaseLayer.Query<DiscordBitch>().FirstOrDefault(x => x.DiscordId == cmd.User.Id.ToString());
+
+        if(u1 is null) {
+            var error_embed = new Discord.EmbedBuilder()
+                .WithTitle($"Please register to send money")
+                .WithColor(Discord.Color.Red)
+                .WithCurrentTimestamp();
+
+            await cmd.RespondAsync(embed: error_embed.Build(), ephemeral: true);
+            return;
+        }
+
+        var u1b = DatabaseLayer.Query<Bitch>().FirstOrDefault(x => x.Id == u1.BitchId);
+
+        var u2 = DatabaseLayer.Query<DiscordBitch>().FirstOrDefault(x => x.DiscordId == user.Id.ToString());
+
+        if(u2 is null) {
+            var error_embed = new Discord.EmbedBuilder()
+                .WithTitle($"User {user.GlobalName} needs to register")
+                .WithColor(Discord.Color.Red)
+                .WithCurrentTimestamp();
+
+            await cmd.RespondAsync(embed: error_embed.Build(), ephemeral: true);
+            return;
+        }
+
+        var u2b = DatabaseLayer.Query<Bitch>().FirstOrDefault(x => x.Id == u2.BitchId);
+
+        if(u1b!.Money < amount) {
+            var error_embed = new Discord.EmbedBuilder()
+                .WithTitle($"You do not have enough money in your account")
+                .WithDescription($"Balance: {u1b.Money}, U a brokie")
+                .WithColor(Discord.Color.Red)
+                .WithCurrentTimestamp();
+
+            await cmd.RespondAsync(embed: error_embed.Build(), ephemeral: true);
+            return;
+        }
+
+        u1b!.Money -= amount;
+        DatabaseLayer.Update(u1b);
+
+        u2b!.Money += amount;
+        DatabaseLayer.Update(u2b);
+
+        var embed = new Discord.EmbedBuilder()
+            .WithTitle($"Send {amount} to account {u2.BitchId}")
+            .WithDescription($"New balance: {u1b.Money}")
+            .WithColor(Discord.Color.Blue)
+            .WithCurrentTimestamp();
+
+        await cmd.RespondAsync(embed: embed.Build(), ephemeral: true);
+    }
+
+    private static Task Log(Discord.LogMessage msg) {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
